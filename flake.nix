a1df31a445d474dfd076d51ae0471b61458a7c9f
KurikoMoe <kurikomoe@gmail.com>
2025-06-08 18:03:39 +0800

    fix(action): openapi

--- flake.nix
+++ flake.nix
@@ -4,34 +4,27 @@
   inputs = {
     flake-parts.url = "github:hercules-ci/flake-parts";
 
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
-    nixpkgs-unstable.url = "github:NixOS/nixpkgs";
+    devenv.url = "github:cachix/devenv/latest";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
 
-    devenv = {
-      url = "github:cachix/devenv";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-
-    nixpkgs-python = {
-      url = "github:cachix/nixpkgs-python";
-      inputs = {nixpkgs.follows = "nixpkgs";};
-    };
-
-    kuriko-nur = {
-      url = "github:kurikomoe/nur-packages";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
+    kuriko-nur.url = "github:kurikomoe/nur-packages";
+    kuriko-nur.inputs.nixpkgs.follows = "nixpkgs";
   };
 
   nixConfig = {
     substituters = [
-      https://mirrors.ustc.edu.cn/nix-channels/store
-      https://mirrors.tuna.tsinghua.edu.cn/nix-channels/store
-      https://nix-community.cachix.org
+      "https://cache.nixos.org"
+      "https://nixpkgs-python.cachix.org"
+      "https://nix-community.cachix.org"
+      "https://mirrors.ustc.edu.cn/nix-channels/store"
+      "https://mirrors.tuna.tsinghua.edu.cn/nix-channels/store"
+      "https://kurikomoe.cachix.org"
     ];
     trusted-public-keys = [
       "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+      "nixpkgs-python.cachix.org-1:hxjI7pFxTyuTHn2NkvWCrAUcNZLNS3ZAvfYNuYifcEU="
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+      "kurikomoe.cachix.org-1:NewppX3NeGxT8OwdwABq+Av7gjOum55dTAG9oG7YeEI="
     ];
     extra-trusted-public-keys = "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw=";
     extra-substituters = "https://devenv.cachix.org";
@@ -39,12 +32,8 @@
 
   outputs = inputs @ {flake-parts, ...}:
     flake-parts.lib.mkFlake {inherit inputs;} {
-      imports = [
-        inputs.devenv.flakeModule
-      ];
-
+      imports = [inputs.devenv.flakeModule];
       systems = ["x86_64-linux" "aarch64-linux" "aarch64-darwin" "x86_64-darwin"];
-
       perSystem = {
         config,
         self',
@@ -121,13 +110,8 @@
 
             trufflehog = {
               enable = true;
-              entry = let
-                script = pkgs.writeShellScript "precommit-trufflehog" ''
-                  set -e
-                  ${pkgs.trufflehog}/bin/trufflehog --no-update git "file://$(git rev-parse --show-toplevel)" --since-commit HEAD --results=verified --fail
-                '';
-              in
-                builtins.toString script;
+              entry = builtins.toString inputs.kuriko-nur.legacyPackages.${system}.precommit-trufflehog;
+              stages = ["pre-push" "pre-commit"];
             };
 
             # pylint.enable = true;
